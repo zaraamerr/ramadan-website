75e99c344240adfcfb73e3bc6246d71c4ca49211
zaraamerr <zaraa.amerr@gmail.com>
2024-03-16 15:39:33 -0400

    random commit

--- weather/src/App.js
+++ weather/src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 function App() {
 
 
-  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=brooklyn&appid=cfe744556bb46aca137032729fe4ddeb'
+  
   return (
     <div className="App">
     </div>
