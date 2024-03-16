83f01db6c0d479be2b00546f53184479f9c0e2a0
zaraamerr <zaraa.amerr@gmail.com>
2024-03-16 15:32:24 -0400

    set up react app waiting for api key to validate

---
+++ weather/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+function App() {
+
+
+  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=brooklyn&appid=cfe744556bb46aca137032729fe4ddeb'
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
